501cd0763651dca5bb4b5cf1266fb27ffba23d17
samade747 <samad.e747@gmail.com>
2024-02-06 23:37:26 +0500

    1137PM 6 2 2024

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
+
+const Container = styled.div``;
+
+const Main = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* menu */}
+    {/* main */}
+    </Container>
   );
 }
 
