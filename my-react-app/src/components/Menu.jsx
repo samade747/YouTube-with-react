8dee015bfcc3ddd4e0ae6b4799f58b131c9f4a33
samade747 <samad.e747@gmail.com>
2024-02-07 14:36:21 +0500

    236pm 722024

--- my-react-app/src/components/Menu.jsx
+++ my-react-app/src/components/Menu.jsx
@@ -1,35 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import youtube from "../img/logoimg.jpg"
+import logoimg from "../img/logoimg.jpg"
 
 
 const Container = styled.div`
 flex:2;
 background-color: #202020;
 height: 100vh;
-color: white;` 
+color: white;
+` ;
 
 const Wrapper = styled.div`
-    padding: 10px 26px;`
+    padding: 10px 26px;
+`;
 
 const Logo = styled.div`
     display: flex;
     align-items: center;
     gap: 5px;    
-`
+`;
 
-const lmg = styled.img`
-    height: 25px
-`
+const Img = styled.img`
+    width: 2px;
+    height: 2px;
+`;
 
 const Menu = () => {
     return(
     <Container>
     <Wrapper>
-        <logo>
-            <Img src="../img/logoimg.jpg">
-            youtube
-        </logo>
+        <Logo>
+            <img src={logoimg} width={80} alt="logo" />
+            
+        </Logo>
     </Wrapper>
     </Container>
     
