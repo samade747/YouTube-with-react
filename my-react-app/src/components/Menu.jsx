2741f1b52a2e3aa628ba508a6f416e0ba555ac8a
samade747 <samad.e747@gmail.com>
2024-02-09 15:54:29 +0500

    354pm 9 2 2024

--- my-react-app/src/components/Menu.jsx
+++ my-react-app/src/components/Menu.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import logoimg from "../img/logoimg.jpg"
 import HomeIcon from '@mui/icons-material/Home';
-
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import HistoryIcon from '@mui/icons-material/History';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 
 const Container = styled.div`
 flex:2;
@@ -28,10 +32,12 @@ const Img = styled.img`
 `;
 
 const Item = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 20px;
-    cursor: pointer;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+gap: 5px;
+cursor: pointer;
+padding: 7.5px;
 
 `;
 
@@ -39,12 +45,24 @@ const Menu = () => {
     return(
     <Container>
     <Wrapper>
+        <Item>
         <Logo>
             <img src={logoimg} width={80} alt="logo" />            
         </Logo>
-        <Item>
-            <HomeIcon />
-            Home
+        <div>
+        <HomeIcon /> Home
+        </div>
+
+        <div><ExploreIcon /> Explore</div>    
+            
+        <div><SubscriptionsIcon /> Subscription</div>        
+            
+        <div><LibraryAddIcon /> Library</div>           
+        
+        <div><HistoryIcon />History</div>
+
+        <div><LibraryMusicIcon /> Music</div>
+
         </Item>
     </Wrapper>
     </Container>
