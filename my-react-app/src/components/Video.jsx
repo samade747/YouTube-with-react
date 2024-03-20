20d93efbe17582b4568919ed7973ea8fa9b8a339
samade747 <samad.e747@gmail.com>
2024-03-20 17:18:34 +0500

    518pm 20 3 2024

--- my-react-app/src/components/Video.jsx
+++ my-react-app/src/components/Video.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+    display: flex;
+    gap: 24px;
+
+`;
+
+const Content = styled.div`
+    flex: 5;
+`;
+
+const Recommendation = styled.div`
+    flex: 2;
+`;
+
 // 4419
 
+const Video = () => {
+
+    return <Container>
+        <Content>content</Content>
+        <Content>recommndation</Content>
+    </Container>
+};
+
+export default Video
